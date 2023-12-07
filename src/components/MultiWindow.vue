1af97d3c46256fbc273d519e6aefb8bbb39f3b3c
Dark Mode <darkkmodee00@gmail.com>
2023-12-07 14:22:44 -0500

    Rework

---
+++ src/components/MultiWindow.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <video :srcObject="videoStream" :width="videoWidth" :height="videoHeight" autoplay
+      :style="{ transform: 'translate(' + translateX + 'px,' + translateY + 'px)' }"></video>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount, onBeforeMount } from 'vue';
+
+let videoWidth = ref(0);
+let videoHeight = ref(0);
+let videoStream: any = ref(null);
+let translateX = ref(0);
+let translateY = ref(0);
+
+onBeforeMount(() => {
+  initCamera()
+});
+
+onMounted(() => {
+  setInterval(setScreenDetails, 10);
+});
+
+onBeforeUnmount(() => {
+  if (videoStream.value) {
+    const tracks = videoStream.value.getTracks();
+    tracks.forEach((track: any) => track.stop());
+  }
+});
+
+const initCamera = async () => {
+  try {
+    await navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      videoWidth.value = window.screen.availWidth;
+      videoHeight.value = window.screen.availHeight;
+      videoStream.value = stream;
+    })
+  } catch (error) {
+    console.error('Error accessing webcam:', error);
+  }
+};
+
+function setScreenDetails() {
+  translateX.value = -window.screenX
+  translateY.value = -window.screenY
+}
+
+</script>
+
+<style scoped lang="sass">
+</style>
